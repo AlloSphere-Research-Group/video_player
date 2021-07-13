ce6f674c4594b654410bc266e091d1c6e19e0a04
Andres Cabrera <mantaraya36@gmail.com>
2021-07-13 09:50:31 -0700

    Refactored code and cleanup. Not working but building on windows.

--- src/al_VideoPlayer.cpp
+++ src/al_VideoPlayer.cpp
@@ -1,49 +1,4 @@
 
-
-/*	Allocore --
-        Multimedia / virtual environment application class library
-
-        Copyright (C) 2009. AlloSphere Research Group, Media Arts & Technology,
-   UCSB. Copyright (c) 2015-2016 Donghao Ren Copyright (C) 2012. The Regents of
-   the University of California. All rights reserved.
-
-        Redistribution and use in source and binary forms, with or without
-        modification, are permitted provided that the following conditions are
-   met:
-
-                Redistributions of source code must retain the above copyright
-   notice, this list of conditions and the following disclaimer.
-
-                Redistributions in binary form must reproduce the above
-   copyright notice, this list of conditions and the following disclaimer in the
-                documentation and/or other materials provided with the
-   distribution.
-
-                Neither the name of the University of California nor the names
-   of its contributors may be used to endorse or promote products derived from
-                this software without specific prior written permission.
-
-        THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
-   IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-        IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-   PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
-   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
-   OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
-   OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
-   ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-
-        File description:
-
-
-        File author(s):
-        Donghao Ren 2015-2016
-        Andrés Cabrera 2017 mantaraya36@gmail.com
-*/
-
 #include <atomic>
 #include <condition_variable>
 #include <iostream>
@@ -52,277 +7,11 @@
 #include <string>
 #include <thread>
 
-extern "C" {
-#include <libavcodec/avcodec.h>
-#include <libavformat/avformat.h>
-#include <libavutil/pixfmt.h>
-#include <libswscale/swscale.h>
-}
-
-// Hacks for older ffmpeg
-#if LIBAVCODEC_VERSION_INT < AV_VERSION_INT(55, 28, 1)
-#define AV_PIX_FMT_RGBA PIX_FMT_RGBA
-#define av_frame_alloc avcodec_alloc_frame
-#define av_frame_free avcodec_free_frame
-#endif
-
-#include "al/app/al_App.hpp"
-#include "al/types/al_SingleRWRingBuffer.hpp"
 //#include "allocore/system/al_Thread.hpp"
 
-namespace al {
+#include "al_VideoPlayer.hpp"
 
-// TODO better handling of buffers and audio/video de-interleaving. Currently
-// works OK with mp4 but fails with some mov files.
-// TODO more informed choice of buffer sizes for mAudioBuffer and mPictureBuffer
-// TODO add options for choosing video streams
-// TODO add options to choose audio resampling
-// TODO add a function to query if video file is done
-
-class VideoFileReader {
-public:
-  typedef enum { SYNC_AUDIO, SYNC_VIDEO, SYNC_INTERNAL, SYNC_AUTO } SyncMode;
-
-  typedef enum { PLAY_ONESHOT, PLAY_LOOPING } PlayMode;
-
-  /**
-   *
-   * \brief A class that wraps libavcodec and libavformat (ffmpeg) for video
-   * decoding
-   * \param path to video file
-   * \param syncMode determines the sync
-   * "clock" for media
-   * \param outputWidth if different to -1 the output's witdh
-   * will be resized to this value
-   * \param outputHeight if different to -1 the
-   * output's height will be resized to this value
-   *
-   * This class can read the video file in three modes:
-   * SYNC_AUDIO makes the audio request drive the decoding. Whenever
-   * audio is read from the buffers using readAudio(), this triggers an update
-   * of the current video frame when needed. Use this mode when you want to
-   * playback audio and video in sync.
-   *
-   * SYNC_VIDEO makes the request for video frames drive the decoding. When
-   * nextFrame() is called, the next frame is decoded. This allows getting
-   * frames sequentially at an arbitrary rate different to the original
-   * video's rate.
-   *
-   * SYNC_INTERNAL makes the computer's internal clock drive the decoding.
-   * A separate thread watches the clock and updates the current frame
-   * at the rate requested by the frame rate. This might mean that frames
-   * might be missed or used twice as the process is asynchronous, but
-   * guarantees that the duration and playback speed of the video is accurate
-   * and consistent.
-   *
-   * SYNC_AUTO determines the mode from the contents of the movie file. If an
-   * audio track is present in the file, SYNC_AUDIO is used. If no audio is
-   * present SYNC_INTERNAL is used.
-   */
-  VideoFileReader(const char *path, SyncMode syncMode = SYNC_AUTO,
-                  int outputWidth = -1, int outputHeight = -1);
-
-  virtual ~VideoFileReader() { cleanup(); }
-
-  void start();
-
-  void stop();
-
-  ///
-  /// \brief width
-  /// \return The width of the source video frame
-  ///
-  virtual int width() const { return mPictureCodecContext->width; }
-
-  ///
-  /// \brief height
-  /// \return The height of the source video frame
-  ///
-  virtual int height() const { return mPictureCodecContext->height; }
-
-  virtual double fps() { return mFps; }
-
-  virtual double duration() { return mDuration; }
-
-  int currentFrame() { return mCurrentVideoFrame.load(); }
-
-  ///
-  /// \brief nextFrame
-  /// \return returns true if a new frame was decoded.
-  /// Call this function only if the file has no audio track, to force
-  /// reading of frames.
-  virtual bool nextFrame();
-
-  ///
-  /// \brief readAudio
-  /// \param channel
-  /// \param buf
-  /// \param numElements
-  /// \return
-  ///
-  /// When syncing to audio you need to call read audio for at least channel 0,
-  /// the free space in this buffer determines whether frames should be read.
-  /// Even if you don't use this channel, make sure you call this function
-  /// for it to flush its buffer.
-  int readAudio(int channel, float *buf, size_t numElements);
-
-  ///
-  /// \brief Set pixel buffer where video frames are written
-  /// \param buffer
-  ///
-  /// You need to call this only once, and the pixel buffer will be updated
-  /// when required. If you call this function, the video will not be
-  /// decoded to the internal buffer, so calling pixels() will return an
-  /// empty buffer. You can reset usage to the internal buffer by passing
-  /// nullptr as argument.
-  virtual void setPixelBuffer(void *buffer);
-
-  ///
-  /// \brief returns a pointer to the current frame's pixel data
-  ///
-  /// Note that this is not inherently safe as the reader thread might be
-  /// writing to the array. Make sure you call this synchronously
-  ///
-  /// \return pointer to frame pixel data
-  ///
-  virtual const void *pixels() const {
-    if (assigned_buffer_) {
-      return assigned_buffer_;
-    } else {
-      return mPictureBuffer[mPicBufRead];
-    }
-  }
-
-  ///
-  /// \brief Move the movie read location to specified frame
-  ///
-  virtual void seek(int64_t frame);
-
-  void setPlayMode(PlayMode mode) { mPlayMode = mode; }
-
-private:
-  void intializeVideoStream();
-
-  void cleanup();
-
-  inline void readVideoFromPacket(VideoFileReader *obj, AVPacket &packet,
-                                  AVFrame *frame) {
-    int frame_finished;
-    /*if (obj->mSkipFrames.load() > 0) {
-        obj->mSkipFrames--;
-    } else */
-    {
-      avcodec_decode_video2(obj->mPictureCodecContext, frame, &frame_finished,
-                            &packet);
-      if (frame_finished) {
-        std::unique_lock<std::mutex> videoLock(obj->mVideoFrameMutex);
-        sws_scale(obj->mScalingContext, (uint8_t const *const *)frame->data,
-                  frame->linesize, 0, obj->mPictureCodecContext->height,
-                  obj->frame_rgb_->data, obj->frame_rgb_->linesize);
-        //                std::cout << "Video frame: " <<
-        //                obj->mCurrentVideoFrame.load() << std::endl;
-        obj->mCurrentVideoFrame++;
-
-        // What should be done is frame is not finished at this point?
-      }
-    }
-  }
-
-  ///
-  /// \brief static function for reader thread (for SYNC_AUDIO mode)
-  ///
-  static void readFunction(VideoFileReader *obj);
-
-  ///
-  /// \brief static function for picture only reader thread (for SYNC_VIDEO
-  /// mode)
-  ///
-  static void readFunctionNoAudio(VideoFileReader *obj);
-
-  void ffmpeg_initialize();
-
-  AVFormatContext *mFormatContext;
-  AVDictionary *mOptionsDict;
-  SyncMode mSyncMode;
-  PlayMode mPlayMode;
-
-  // Video stream
-  AVCodecContext *mPictureCodecContext;
-  bool mCodecContextOpened; // True if video stream opened succesfully
-  AVCodec *mPictureCodec;
-  int mVideoStreamIndex; // Index to video stream
-  struct SwsContext *mScalingContext;
-  int64_t mTimeBase;
-  double mFps;
-  double mDuration;
-  int mOutputWidth, mOutputHeight; // width and height of the output frame
-
-  // Audio stream
-  AVCodecContext *mAudioCodecContext;
-  bool mAudioCodecContextOpened; // True if audio stream opened succesfully
-  AVCodec *mAudioCodec;
-  int mAudioStreamIndex; // Index to audio stream
-  int mSampleRate;
-  int mAudioChannels;
-
-  // Picture decoding buffers
-  AVPacket packet;
-  AVFrame *frame_;
-  AVFrame *frame_rgb_;
-  uint8_t *assigned_buffer_ =
-      nullptr;    // if nullptr, then use the picture ring buffer
-  int num_bytes_; // Size of the video frame buffers
-
-  std::atomic<uint64_t> mCurrentVideoFrame;
-  std::atomic<int64_t> mSeek;
-  std::atomic<int> mSkipFrames;
-
-  std::mutex mVideoFrameMutex;
-  std::mutex mLock;
-  std::condition_variable mCondVar;
-  std::thread *mReaderThread;
-  std::atomic<bool> mRunning;
-
-  SingleRWRingBuffer mAudioBuffer[2] = {{8192 * 8}, {8192 * 8}};
-  std::vector<uint8_t *> mPictureBuffer;
-  int mPicBufSize;
-  int mPicBufWrite = 1, mPicBufRead = 0;
-};
-
-///
-/// \brief The VideoTexture class provides a simple way to read video straight
-/// into a texture
-///
-///
-class VideoTexture : public Texture {
-public:
-  VideoTexture(std::string fileName = "test.avi",
-               VideoFileReader::SyncMode syncMode = VideoFileReader::SYNC_AUTO,
-               int textureWidth = -1, int textureHeight = -1);
-
-  void start() { mVideoReader.start(); }
-
-  void stop() { mVideoReader.stop(); }
-
-  int width() { return mVideoReader.width(); }
-
-  int height() { return mVideoReader.height(); }
-
-  void readFrame();
-
-  int readAudio(int channel, float *buffer, int numElements);
-
-  void seek(int frame);
-
-  int currentFrame() { return mVideoReader.currentFrame(); }
-
-  void setPlayMode(VideoFileReader::PlayMode mode) {
-    mVideoReader.setPlayMode(mode);
-  }
-
-private:
-  VideoFileReader mVideoReader;
-};
+using namespace al;
 
 // --------------------------- Implementation
 
@@ -838,12 +527,14 @@ VideoTexture::VideoTexture(std::string fileName,
   //    mVideoReader.setPixelBuffer(this->data());
 }
 
-void VideoTexture::readFrame() {
-  //        if (frame >= 0 && frame != currentFrame()) {
-  //            mVideoReader.seek(frame);
-  //        }
+void VideoTexture::readFrame(uint64_t framenum) {
   // FIXME need to make sure frame rate at which this is called matches video
   // frame rate
+  if (framenum != UINT64_MAX) {
+    if (framenum >= 0 && framenum != currentFrame()) {
+      mVideoReader.seek(framenum);
+    }
+  }
   if (mVideoReader.nextFrame()) {
     // FIXME we need to cpy the data to the texture
     //    memcpy(this->array().data.ptr, mVideoReader.pixels(),
@@ -858,7 +549,5 @@ int VideoTexture::readAudio(int channel, float *buffer, int numElements) {
 }
 
 void VideoTexture::seek(int frame) { mVideoReader.seek(frame); }
-
-} // namespace al
 
 // -------------------------------------
--- src/videoplayback.cpp
+++ src/videoplayback.cpp
@@ -1,233 +1,147 @@
 
-#include "alloutil/al_OmniApp.hpp"
-#include "allocore/system/al_Time.hpp"
+#include "al/system/al_Time.hpp"
 
-#include "al_VideoPlayer.cpp"
-
-#include "Cuttlebone/Cuttlebone.hpp"
-
-
-typedef struct {
-	int frameNum;
-} SharedState;
-
-//#define BUILD_RENDERER
+#include "videoplayback.hpp"
 
 using namespace al;
 
-class MyApp: public OmniApp {
-public:
-	MyApp(std::string name = "omniapp", bool slave = false)
+void MyApp::onCreate() {
+  //	auto& s = shader();
+  //    s.begin();
+  //    s.uniform("texture", 1.0);
+  //    s.uniform("lighting", 0.1);
+  //    s.end();
+  if (mSideBySide) {
+    mQuadL.reset();
+    mQuadL.primitive(Mesh::TRIANGLES);
+    mQuadL.vertex(-1, -1, 0);
+    mQuadL.texCoord(0, 1);
+    mQuadL.vertex(1, -1, 0);
+    mQuadL.texCoord(0.5, 1);
+    mQuadL.vertex(1, 1, 0);
+    mQuadL.texCoord(0.5, 0);
+    mQuadL.vertex(-1, 1, 0);
+    mQuadL.texCoord(0, 0);
 
-#ifdef BUILD_RENDERER
-#else
-	    :
-	mMaker("192.168.10.0")
-#endif
-	{
-		mPlaying = false;
-#ifdef BUILD_RENDERER
-		mTaker.start();
-#else
-//		mMaker.start();
-#endif
+    mQuadR.reset();
+    mQuadR.primitive(Mesh::TRIANGLES);
+    mQuadR.vertex(-1, -1, 0);
+    mQuadR.texCoord(0.5, 1);
+    mQuadR.vertex(1, -1, 0);
+    mQuadR.texCoord(1, 1);
+    mQuadR.vertex(1, 1, 0);
+    mQuadR.texCoord(1, 0);
+    mQuadR.vertex(-1, 1, 0);
+    mQuadR.texCoord(0.5, 0);
+  } else {
+    mQuadL.reset();
+    mQuadL.primitive(Mesh::TRIANGLES);
+    mQuadL.vertex(-1, -1, 0);
+    mQuadL.texCoord(0, 1);
+    mQuadL.vertex(1, -1, 0);
+    mQuadL.texCoord(1, 1);
+    mQuadL.vertex(1, 1, 0);
+    mQuadL.texCoord(1, 0);
+    mQuadL.vertex(-1, 1, 0);
+    mQuadL.texCoord(0, 0);
+  }
 
-	}
+  //  mVideoTexture.print();
+  mVideoTexture.setPlayMode(VideoFileReader::PLAY_ONESHOT);
 
-    virtual ~MyApp()
-	{
-#ifdef BUILD_RENDERER
-		mTaker.stop();
-#else
-		mMaker.stop();
-#endif
-
-	}
-
-    virtual bool onCreate() override;
-    virtual void onDraw(Graphics& gl) override;
-	virtual void onAnimate(al_sec dt) override;
-#ifdef BUILD_RENDERER
-#else
-    virtual void onSound(AudioIOData &io) override;
-    virtual void onMessage(osc::Message& m) override;
-	virtual bool onKeyDown(const Keyboard& k) override;
-#endif
-
-	SharedState &state() {return mState;}
-private:
-//	VideoTexturer mVideoTexture {"/data/andres/PlieguesALTh264.mov"};
-//    VideoTexturer mVideoTexture {"/data/andres/Pliegues1024.mp4"};
-//    VideoTexture mVideoTexture {"/home/andres/Probably-3mins-final.mp4"};
-    VideoTexture mVideoTexture {"/home/andres/Documents/07 Proyectos y trabajos/06 Musica/geminiflux/CCT1.mp4"};
-//    VideoTexturer mVideoTexture {"/media/andres/LaCie_Mac_Only/_AlloArchive/AlloSphere Archive_032714/1-New-UK-KucheraMorin.mov", true};
-//    VideoTexturer mVideoTexture {"/media/andres/LACIE SHARE/brain-test.mp4", VideoFileReader::SYNC_AUDIO};
-//    VideoTexturer mVideoTexture {"/home/andres/Videos/Webcam/2014-12-08-210226.webm"};
-    bool mSideBySide = true;
-	Mesh mQuadL, mQuadR;
-	SharedState mState;
-	bool mPlaying;
-
-#ifdef BUILD_RENDERER
-	cuttlebone::Taker<SharedState> mTaker;
-#else
-	cuttlebone::Maker<SharedState> mMaker;
-#endif
-};
-
-bool MyApp::onCreate()
-{
-	OmniApp::onCreate();
-	auto& s = shader();
-    s.begin();
-    s.uniform("texture", 1.0);
-    s.uniform("lighting", 0.1);
-    s.end();
-    if (mSideBySide) {
-        mQuadL.reset();
-        mQuadL.primitive(Graphics::QUADS);
-        mQuadL.vertex(-1, -1, 0);
-        mQuadL.texCoord(0, 1);
-        mQuadL.vertex( 1, -1, 0);
-        mQuadL.texCoord(0.5, 1);
-        mQuadL.vertex( 1,  1, 0);
-        mQuadL.texCoord(0.5, 0);
-        mQuadL.vertex(-1,  1, 0);
-        mQuadL.texCoord(0, 0);
-
-        mQuadR.reset();
-        mQuadR.primitive(Graphics::QUADS);
-        mQuadR.vertex(-1, -1, 0);
-        mQuadR.texCoord(0.5, 1);
-        mQuadR.vertex( 1, -1, 0);
-        mQuadR.texCoord(1, 1);
-        mQuadR.vertex( 1,  1, 0);
-        mQuadR.texCoord(1, 0);
-        mQuadR.vertex(-1,  1, 0);
-        mQuadR.texCoord(0.5, 0);
-    } else {
-        mQuadL.reset();
-        mQuadL.primitive(Graphics::QUADS);
-        mQuadL.vertex(-1, -1, 0);
-        mQuadL.texCoord(0, 1);
-        mQuadL.vertex( 1, -1, 0);
-        mQuadL.texCoord(1, 1);
-        mQuadL.vertex( 1,  1, 0);
-        mQuadL.texCoord(1, 0);
-        mQuadL.vertex(-1,  1, 0);
-        mQuadL.texCoord(0, 0);
-    }
-
-	mVideoTexture.print();
-    mVideoTexture.setPlayMode(VideoFileReader::PLAY_ONESHOT);
-
-	state().frameNum = 0;
-	Window::fps(30);
-#ifdef BUILD_RENDERER
-	omni().mode(OmniStereo::MONO);
-#else
-	omniEnable(false);
-    initAudio(44100);
-#endif
+  state().frameNum = 0;
+  // TODO set fps according to video file
+  //	Window::fps(30);
 }
 
-void MyApp::onAnimate(al_sec dt)
-{
-#ifdef BUILD_RENDERER
-	mTaker.get(mState);
-	mVideoTexture.readFrame(state().frameNum);
-#else
-//    std::cout << dt << std::endl;
+void MyApp::onAnimate(al_sec dt) {
+  if (isPrimary()) {
+    //    std::cout << dt << std::endl;
     //	mTaker.get(mState);
+    static int frame = 0;
 
-    if (frame == 5)
-    {
-//        osc::Send sender(9090, "localhost");
-//        sender.send("/play", 1.0f);
+    if (frame == 5) {
+      //        osc::Send sender(9090, "localhost");
+      //        sender.send("/play", 1.0f);
 
-        mVideoTexture.start();
-        mPlaying = true;
+      mVideoTexture.start();
+      mPlaying = true;
     }
-//    if (frame == 176) {
+    frame++;
+    //    if (frame == 176) {
     //    }
     if (mPlaying) {
-        mVideoTexture.readFrame();
-        state().frameNum++;
+      mVideoTexture.readFrame();
+      state().frameNum++;
 
-        //	mMaker.set(state());
+      //	mMaker.set(state());
     }
-//    std::cout << (int) frame - mVideoTexture.currentFrame()<< std::endl;
-#endif
+    //    std::cout << (int) frame - mVideoTexture.currentFrame()<< std::endl;
+
+  } else {
+    mVideoTexture.readFrame(state().frameNum);
+  }
 }
 
-void MyApp::onDraw(Graphics &g)
-{
-#ifdef BUILD_RENDERER
-	//	mVideoTexture.readFrame();
-	g.pushMatrix();
-	g.rotate(-90, 0, 1, 0);
-	//	g.scale(0.3);
-	mVideoTexture.bind();
-	g.draw(mQuad);
-	mVideoTexture.unbind();
-	g.popMatrix();
-#else
+void MyApp::onDraw(Graphics &g) {
+
+  if (isPrimary()) {
+
     mVideoTexture.bind();
     g.pushMatrix();
     g.translate(-1, 0.1, -2);
     g.draw(mQuadL);
     g.popMatrix();
     if (mSideBySide) {
-        g.pushMatrix();
-        g.translate(1, -0.1, -2);
-        g.draw(mQuadR);
-        g.popMatrix();
-	}
-	mVideoTexture.unbind();
-//    std::cout << state().frameNum - mVideoTexture.currentFrame() << std::endl;
-#endif
+      g.pushMatrix();
+      g.translate(1, -0.1, -2);
+      g.draw(mQuadR);
+      g.popMatrix();
+      mVideoTexture.unbind();
+    } else {
+      mVideoTexture.readFrame();
+      g.pushMatrix();
+      g.rotate(-90, 0, 1, 0);
+      //	g.scale(0.3);
+      mVideoTexture.bind();
+      g.draw(mQuadL);
+      mVideoTexture.unbind();
+      g.popMatrix();
+    }
+    //    std::cout << state().frameNum - mVideoTexture.currentFrame() <<
+    //    std::endl;
+  }
 }
 
-#ifdef BUILD_RENDERER
-
-#else
-void MyApp::onSound(AudioIOData &io)
-{
+void MyApp::onSound(AudioIOData &io) {
+  if (isPrimary()) {
     float buffer[8192];
     int numFileChnls = 2;
     for (int i = 0; i < numFileChnls; i++) {
-        size_t elementsRead = mVideoTexture.readAudio(i, buffer, io.framesPerBuffer());
-        if (elementsRead > 0) {
-            memcpy(io.outBuffer(i), buffer, elementsRead * sizeof(float));
-        }
-//        if (elementsRead < io.framesPerBuffer()) {
-//            std::cout << "buffer underrun! read " << elementsRead <<  std::endl;
-//        }
+      size_t elementsRead =
+          mVideoTexture.readAudio(i, buffer, io.framesPerBuffer());
+      if (elementsRead > 0) {
+        memcpy(io.outBuffer(i), buffer, elementsRead * sizeof(float));
+      }
+      //        if (elementsRead < io.framesPerBuffer()) {
+      //            std::cout << "buffer underrun! read " << elementsRead <<
+      //            std::endl;
+      //        }
     }
+  }
 }
 
-void MyApp::onMessage(osc::Message &m)
-{
+void MyApp::onMessage(osc::Message &m) {}
 
-}
-
-bool MyApp::onKeyDown(const Keyboard &k)
-{
-	if (k.key() == 32) {
-		mPlaying = true;
-		osc::Send sender(9090, "localhost");
-		sender.send("/play", 1.0f);
-	} else if (k.keyAsNumber() == 0) {
-        std::cout << "play again" << std::endl;
-        mVideoTexture.seek(0);
-        mVideoTexture.start();
-    }
-	return true;
-}
-#endif
-
-int main(int argc, char *argv[])
-{
-	MyApp().start();
-    return 0;
+bool MyApp::onKeyDown(const Keyboard &k) {
+  if (k.key() == 32) {
+    mPlaying = true;
+    osc::Send sender(9090, "localhost");
+    sender.send("/play", 1.0f);
+  } else if (k.keyAsNumber() == 0) {
+    std::cout << "play again" << std::endl;
+    mVideoTexture.seek(0);
+    mVideoTexture.start();
+  }
+  return true;
 }
